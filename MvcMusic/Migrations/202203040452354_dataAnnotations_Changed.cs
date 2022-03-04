153164eed62171cf9ae67d0e46fa1a2faffeaf90
Osman Ünüvar <osmanunuvar@gmail.com>
2022-03-04 07:53:55 +0300

    Data annotations changed

---
+++ MvcMusic/Migrations/202203040452354_dataAnnotations_Changed.cs
@@ -0,0 +1,16 @@
+namespace MvcMusic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class dataAnnotations_Changed : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- MvcMusic/ViewModels/CreateMusicViewModel.cs
+++ MvcMusic/ViewModels/CreateMusicViewModel.cs
@@ -9,15 +9,13 @@ namespace MvcMusic.ViewModels
 {
     public class CreateMusicViewModel
     {
-        [Required, StringLength(60, MinimumLength = 3)]
+        [Required, StringLength(60, MinimumLength = 3),Display(Name ="Şarkı Adı")]
         public string Title { get; set; }
-        [MaxLength(50)]
-        [Required]
+        [MaxLength(50),Required,Display(Name ="Sanatçı")]
         public string Artist { get; set; }
-        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        [Display(Name = "Çıkış Tarihi"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        [MaxLength(50)]
-        [Required]
+        [MaxLength(50),Required,Display(Name ="Şarkı Türü")]
         public string Genre { get; set; }
 
         public IEnumerable<SelectListItem> ListItems { get; set; } 
